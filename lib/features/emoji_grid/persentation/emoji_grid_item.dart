b13f2cbb0174001753ce0e94194191e779d5ce92
linyi517 <1158029271@qq.com>
2024-04-05 18:35:16 +0800

    refactor: extract emoji grid item component

---
+++ lib/features/emoji_grid/persentation/emoji_grid_item.dart
@@ -0,0 +1,36 @@
+import 'package:emoji_room/constants/app_theme.dart';
+import 'package:emoji_room/features/emoji_detail/views/emoji_detail.dart';
+import 'package:emoji_room/features/emoji_grid/application/emoji_service.dart';
+import 'package:emoji_room/features/emoji_grid/domain/emoji.dart';
+import 'package:emoji_room/widgets/bottom_sheet.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EmojiGridItem extends ConsumerWidget {
+  const EmojiGridItem(this.emoji, {super.key});
+  final Emoji emoji;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(AppTheme.emojiRadius),
+      onTap: () => showDetailView(context, emoji),
+      onLongPress: () => shareEmoji(ref, emoji, context),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(AppTheme.emojiRadius),
+          child: Image.file(emoji.file, fit: BoxFit.cover),
+        ),
+      ),
+    );
+  }
+
+  Future<void> shareEmoji(WidgetRef ref, Emoji emoji, BuildContext context) =>
+      ref.read(emojiServiceProvider).shareEmoji(emoji);
+
+  Future<dynamic> showDetailView(BuildContext context, Emoji emoji) {
+    return showCommonModalBottomSheet(
+        context: context, builder: (context) => EmojiDetailView(emoji.id));
+  }
+}
--- lib/features/emoji_grid/persentation/emoji_grid_view.dart
+++ lib/features/emoji_grid/persentation/emoji_grid_view.dart
@@ -1,8 +1,5 @@
-import 'package:emoji_room/constants/app_theme.dart';
-import 'package:emoji_room/features/emoji_detail/views/emoji_detail.dart';
 import 'package:emoji_room/features/emoji_grid/application/emoji_service.dart';
-import 'package:emoji_room/features/emoji_grid/domain/emoji.dart';
-import 'package:emoji_room/widgets/bottom_sheet.dart';
+import 'package:emoji_room/features/emoji_grid/persentation/emoji_grid_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,43 +8,23 @@ class EmojiGridView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final emojis = ref.watch(filteredEmojiListProvider);
+    final emojis = ref.watch(emojiListProvider);
 
     return emojis.when(
       data: (emojis) => RefreshIndicator(
-        onRefresh: () => ref.refresh(filteredEmojiListProvider.future),
+        onRefresh: () => ref.refresh(emojiListProvider.future),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 100),
           itemCount: emojis.length,
           itemBuilder: (context, index) {
             final emoji = emojis[index];
-            return InkWell(
-              borderRadius: BorderRadius.circular(AppTheme.emojiRadius),
-              onTap: () => showDetailView(context, emoji),
-              onLongPress: () => shareEmoji(ref, emoji, context),
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(AppTheme.emojiRadius),
-                  child: Image.file(emoji.file, fit: BoxFit.cover),
-                ),
-              ),
-            );
+            return EmojiGridItem(emoji);
           },
         ),
       ),
       error: (error, stackTrace) => Center(child: Text(error.toString())),
       loading: () => const Center(child: CircularProgressIndicator()),
     );
-  }
-
-  Future<void> shareEmoji(WidgetRef ref, Emoji emoji, BuildContext context) =>
-      ref.read(emojiServiceProvider).shareEmoji(emoji);
-
-  Future<dynamic> showDetailView(BuildContext context, Emoji emoji) {
-    return showCommonModalBottomSheet(
-        context: context,
-        builder: (context) => ProviderScope(child: EmojiDetailView(emoji.id)));
   }
 }
