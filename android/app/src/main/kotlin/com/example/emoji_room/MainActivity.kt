df1a3a0903ce1f3bc91a395ba7b08e0c8484189a
linyi517 <1158029271@qq.com>
2024-10-10 00:38:31 +0800

    feat: rename app package

--- android/app/src/main/kotlin/com/example/emoji_room/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.emoji_room
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/linyi/emoji_room/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.linyi.emoji_room
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
